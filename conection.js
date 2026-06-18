97d1126d2cfc4885dc93a140593a6e376620d79a
isabelllasarabanda-arch <isabelllasarabanda@gmail.com>
2026-06-18 15:38:33 -0500

    Create conection.js

---
+++ conection.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD7wWX8SKvNVGWuAzco8m-qMAkQs57rvRI",
+  authDomain: "pricenice-d6ada.firebaseapp.com",
+  projectId: "pricenice-d6ada",
+  storageBucket: "pricenice-d6ada.firebasestorage.app",
+  messagingSenderId: "847908888955",
+  appId: "1:847908888955:web:16145873d21a550e405dde"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
